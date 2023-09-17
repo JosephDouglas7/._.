37f23949402a701630de7e68976e5706e5c87abc
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:22:19 -0300

    Create área do aluno.c

---
+++ área do aluno.c
@@ -0,0 +1,11 @@
+void aluno(void){ 
+char op; 
+system("clear||cls");
+printf("\n#   --  Área do aluno --     #"); 
+printf("\n# 1.Cadastrar aluno          #"); 
+printf("\n# 2.Alunos cadastrados       #"); 
+printf("\n# 3.Voltar ao menu principal #"); 
+printf("\nQual sua opção?:              "); 
+scanf("%c", &op); 
+getchar();
+}
