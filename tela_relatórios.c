293c831787216ccc0feafc95db272e134545e62e
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:53:50 -0300

    Create tela_relatórios.c

---
+++ tela_relatórios.c
@@ -0,0 +1,10 @@
+void tela_relatórios(void){ 
+ char op;
+ system("clear||cls");
+ printf("\n#### relatórios #####"); 
+ printf("\n6.Adicionar relatórios");  
+ printf("\n7.Voltar ao menu anterior");
+ printf("\nQual sua opção?:");  
+ scanf("%c", &op); 
+ getchar(); 
+} 
