069dbafcf03d690de0350cbfcbcff7c000cb86ea
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:51:48 -0300

    Create tela_sobre.h

---
+++ tela_sobre.h
@@ -0,0 +1,24 @@
+void tela_sobre(void) {
+ system("clear||cls");
+ printf("\n#Universidade Federal do Rio Grande do Norte #"); 
+ printf("\n#Centro de Ensino Superior do Seridó         #"); 
+ printf("\n#Departamento de Computação e Tecnologia     #"); 
+ printf("\n#Disciplina DCT1106 -- Programação           #"); 
+ printf("\n#Projeto Sistema de gestão de despesas       #"); 
+ printf("\n#em uma República de Estudantes              #"); 
+ printf("\n#Developed by @josephdouglas                 #"); 
+ printf("\n# since agost,2023                           #"); 
+ printf("\n##############################################"); 
+ printf("\n#                                            #"); 
+ printf("\n# -- Sistema de gestão de despesas --        #"); 
+ printf("\n#Programa para fazer gestão de despesas em república de estudantes,para fins de agilizamento do trabalho e melhoramento do atendimento para os estudantes da Universidade.#);                                           #"); 
+ printf("\n# Universidade Federal do Rio Grande do Norte#"); 
+ printf("\n#Centro de Ensino Superior do Seridó         #"); 
+ printf("\n#Departamento de Computação e Tecnologia     #"); 
+ printf("\n#Disciplina DCT1106 -- Programação           #"); 
+ printf("\n#Projeto Sistema de gestão de despesas       #"); 
+ printf("\n#em uma República de Estudantes              #"); 
+ printf("\n#Developed by @josephdouglas                 #"); 
+ printf("\n#since agost,2023                            #"); 
+ getchar();
+} 
