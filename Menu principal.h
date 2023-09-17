d3ce414abea8d5c72f74ac87020fc6072ce028ed
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:20:31 -0300

    Create Menu principal.h

---
+++ Menu principal.h
@@ -0,0 +1,22 @@
+void tela_menu_principal(void){ 
+ char op1; 
+ system("clear||cls");
+ printf("\n# Universidade Federal do Rio Grande do Norte #"); 
+ printf("\n#   Centro de Ensino Superior do Seridó     #"); 
+ printf("\n#   Departamento de Computação e Tecnologia #"); 
+ printf("\n#       Disciplina DCT1106 -- Programação   #"); 
+ printf("\n#    Projeto Sistema de gestão de despesas  #"); 
+ printf("\n#           em uma República de Estudantes  #"); 
+ printf("\n#             Developed by @josephdouglas   #"); 
+ printf("\n#             since agost,2023              #"); 
+ printf("\n#    -- Sistema de gestão de despesas --    #"); 
+ printf("\n#                                           #");
+ printf("\n#           1. Aluno                        #"); 
+ printf("\n#           2. Sobre o programa             #"); 
+ printf("\n#           3. Relatórios                   #"); 
+ printf("\n#           4. Avisos da Semana             #"); 
+ printf("\n#           5. Financias                    #");
+ printf("\n Qual sua opção?:");   
+ scanf("%c", &op1); 
+ getchar();
+} 
