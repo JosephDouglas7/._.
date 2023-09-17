849ea584a31a14b2db139de594e7796d6e96f0ce
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:46:51 -0300

    Create alunos_cadastrados.h

---
+++ alunos_cadastrados.h
@@ -0,0 +1,8 @@
+void alunos_cadastrados(void){  
+char op; 
+system("clear||cls");
+printf("\n## Alunos Cadastrados ##"); 
+printf("\n Zé roela             #"); 
+printf("\n Chavez               #");  
+getchar();
+}
