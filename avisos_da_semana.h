1d56002d64a2eb3d0f3ca2c9186885e5c67e328f
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:57:10 -0300

    Create avisos_da_semana.h

---
+++ avisos_da_semana.h
@@ -0,0 +1,8 @@
+void avisos_da_semana(void){  
+char op; 
+system("clear||cls");
+printf("\n### Avisos da Semana ###"); 
+printf("\nFaxina as sextas"); 
+printf("\nVarrer aos sábados");  
+getchar();
+}
