9f6e305a38e0c98c6e594f044707a9670a97c00f
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:55:14 -0300

    Create tela_relatórios2.c

---
+++ tela_relatórios2.c
@@ -0,0 +1,12 @@
+void tela_relatórios2(void){ 
+  char op; 
+  system("clear||cls");
+    printf("\n#### Relatório ####"); 
+   printf("\n8.A república estudantil está em ordem"); 
+   printf("\n9.A república estudantil está precisando de reformas"); 
+    printf("\n10.A república estudantil está fora de ordem");  
+    printf("\n0.Voltar ao menu principal");
+    printf("\nQual sua opção?:"); 
+    scanf("%c", &op); 
+    getchar(); 
+}
