977454104323aaca4771732126bc3d69e740c387
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-17 10:58:20 -0300

    Create financias.c

---
+++ financias.c
@@ -0,0 +1,22 @@
+void financias(void){ 
+ char op; 
+ system("clear||cls");
+ printf("\n# Universidade Federal do Rio Grande do Norte #"); 
+ printf("\n#   Centro de Ensino Superior do Seridó     #"); 
+ printf("\n#   Departamento de Computação e Tecnologia #"); 
+ printf("\n#       Disciplina DCT1106 -- Programação   #"); 
+ printf("\n#    Projeto Sistema de gestão de despesas  #"); 
+ printf("\n#           em uma República de Estudantes  #"); 
+ printf("\n#             Developed by @josephdouglas   #"); 
+ printf("\n#             since agost,2023              #"); 
+ printf("\n#      ----     Financias        ----       #"); 
+ printf("\n#                                           #");
+ printf("\n#           1. Orçamento atual              #"); 
+ printf("\n#           2. Adicionar financiamento      #"); 
+ printf("\n#           3. Financiamentos atuais        #"); 
+ printf("\n#           4. Voltar ao menu principal     #"); 
+ printf("\n#                                           #"); 
+ printf("\n Qual sua opção?:");  
+ scanf("%c", &op); 
+ getchar();
+} 
