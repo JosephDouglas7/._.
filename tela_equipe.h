302f2551c995017e51aab9869ab1026a6014428f
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-24 11:37:33 -0300

    Update tela_equipe.h

--- tela_equipe.h
+++ tela_equipe.h
@@ -1,13 +1,1 @@
-void tela_equipe(void) {
- system("clear||cls");
- printf("\n###########################################");  
- printf("\n# -- Sistema de gestão de despesas --     #"); 
- printf("\n#                                         #"); 
- printf("\n#Este programa foi desenvolvido por:      #"); 
- printf("\n#Joseph Douglas Moura Batista             #");  
- printf("\n#E-mail:joseph.batista.066@ufrn.edu.br    #"); 
- printf("\n#Redes sociais: @josephdouglas            #"); 
- printf("\n#Git:https://github.com/JosephDouglas7/README.md.git  #"); 
-printf("\n#                                          #");    
-printf("\n############################################");  
-}
+
