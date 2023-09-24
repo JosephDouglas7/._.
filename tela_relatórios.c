7c6ad50d13126ea827982c80fa99d8d6b37175ba
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-24 11:37:12 -0300

    Update tela_relatórios.c

--- tela_relatórios.c
+++ tela_relatórios.c
@@ -1,10 +1,1 @@
-void tela_relatórios(void){ 
- char op;
- system("clear||cls");
- printf("\n#### relatórios #####"); 
- printf("\n6.Adicionar relatórios");  
- printf("\n7.Voltar ao menu anterior");
- printf("\nQual sua opção?:");  
- scanf("%c", &op); 
- getchar(); 
-} 
+
