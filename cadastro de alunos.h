bb12212bbdecd4063b6b830693983bcf80b10a97
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-24 11:38:51 -0300

    Update cadastro de alunos.h

--- cadastro de alunos.h
+++ cadastro de alunos.h
@@ -1,11 +1,1 @@
-void cadastrar_aluno(void){  
-char op; 
-system("clear||cls");
-printf("\n# -- Cadastro de alunos --   #");
-printf("\nNome do aluno:                "); 
-printf("\nIdade:                        "); 
-printf("\nCPF:                          "); 
-printf("\nNúmero:                       ");  
-printf("\nNúmero de matricula:          ");  
-getchar();  
-}
+
