1013ad0ed5f516bea1aaa751c86b5bb8ce1bbf85
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-24 11:29:36 -0300

    cadastrar_alunos.c

--- Financiamentos_atuais.h
+++ Financiamentos_atuais.h
@@ -1,8 +1,1 @@
-void Financiamentos_atuais(void){ 
-char op; 
-system("clear||cls");
-printf("\n ---- Financiamentos atuais ----"); 
-printf("\n06/09 - Financiamento de R$ 1000"); 
-printf("\n07/09 - Financiamento de R$ 500 "); 
-getchar();
-}
+
