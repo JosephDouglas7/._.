2c5a1480bead466a35f76ed274d649b176276a12
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-24 11:37:49 -0300

    Update respostas_relatórios2.h

--- respostas_relatórios2.h
+++ respostas_relatórios2.h
@@ -1,8 +1,1 @@
-void respostas_relatórios2(void){  
-system("clear||cls");
-printf("\nA república estudantil está em ordem"); 
-printf("\nA república estudantil está precisando de reformas"); 
-printf("\nA república estudantil está fora de ordem"); 
-printf("\n#   FIM   #"); 
-getchar();
-}
+
