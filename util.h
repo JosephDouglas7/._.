df1ea4ab0e448432e6788f19e55a87deeaa3038f
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-25 21:51:28 -0300

    Update util.h

--- util.h
+++ util.h
@@ -1,6 +1,6 @@
-//Deixei dessa maneira porque estava dando muito erro que eu não conseguia concertar 
-//int valida_nome(char*);
-//int eh_letra(char);
+int valida_nome(char*);
+int eh_letra(char);
 
-//int eh_bissexto(int);
-//int valida_data(int, int, int);
+int eh_bissexto(int);
+int valida_data(int, int, int);
+ 
