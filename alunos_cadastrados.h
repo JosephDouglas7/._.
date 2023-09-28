5f2c0b97f6f016d7b67ea0fee655b89c68dbf376
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-28 15:57:59 -0300

    Update and rename alunos_cadastrados.h to Validador_de_data_de_nascimento.c

--- alunos_cadastrados.h
+++
@@ -1,8 +0,0 @@
-void alunos_cadastrados(void){  
-char op; 
-system("clear||cls");
-printf("\n## Alunos Cadastrados ##"); 
-printf("\n Zé roela             #"); 
-printf("\n Chavez               #");  
-getchar();
-}
