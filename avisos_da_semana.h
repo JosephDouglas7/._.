69a7cfee2a899375b2d50b490f79ea0a0f5a9e72
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-28 15:56:25 -0300

    Validador_de_CPF.c

--- avisos_da_semana.h
+++
@@ -1,8 +0,0 @@
-void avisos_da_semana(void){  
-char op; 
-system("clear||cls");
-printf("\n### Avisos da Semana ###"); 
-printf("\nFaxina as sextas"); 
-printf("\nVarrer aos sábados");  
-getchar();
-}
