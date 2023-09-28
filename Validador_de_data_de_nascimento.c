db2cdfa9616fe99f62b9f787a204eb5f2c4e8660
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-28 15:59:26 -0300

    Update Validador_de_data_de_nascimento.c

--- Validador_de_data_de_nascimento.c
+++ Validador_de_data_de_nascimento.c
@@ -1,1 +1,40 @@
+//Feito com a ajuda do ChatGPT
+#include <stdio.h>
+
+int main() {
+    int dia, mes, ano;
+    printf("Digite sua data de nascimento (dd/mm/aaaa): ");
+    scanf("%d/%d/%d", &dia, &mes, &ano);
+
+    if (ano < 1900 || ano > 2023) {
+        printf("Ano inválido!\n");
+        return 1;
+    }
+
+    if (mes < 1 || mes > 12) {
+        printf("Mês inválido!\n");
+        return 1;
+    }
+
+    if (dia < 1 || dia > 31) {
+        printf("Dia inválido!\n");
+        return 1;
+    }
+
+    if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia == 31) {
+        printf("Dia inválido para o mês especificado!\n");
+        return 1;
+    }
+
+    if (mes == 2) {
+        int bissexto = ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0);
+        if (dia > 29 || (dia == 29 && !bissexto)) {
+            printf("Dia inválido para fevereiro!\n");
+            return 1;
+        }
+    }
+
+    printf("Data válida!\n");
+    return 0;
+}
 
