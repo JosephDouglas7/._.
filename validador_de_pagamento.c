b6bf887287d90891b6712e91bc0fd5feb8062c98
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-29 21:31:48 -0300

    validador_de_pagamento.c

---
+++ validador_de_pagamento.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int validar_pagamento(float valor) {
+    if (valor > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    float valor;
+    printf("Digite o valor do pagamento: ");
+    scanf("%f", &valor);
+
+    if (validar_pagamento(valor)) {
+        printf("Pagamento válido.\n");
+    } else {
+        printf("Pagamento inválido.\n");
+    }
+
+    return 0;
+}
+
