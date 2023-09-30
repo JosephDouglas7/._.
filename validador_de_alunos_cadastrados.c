039149554698f206a3031510a33f2051eb334d0e
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-30 10:26:27 -0300

    validador_de_alunos_cadastrados.c

---
+++ validador_de_alunos_cadastrados.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+// Estrutura para armazenar os dados do aluno
+typedef struct {
+    char nome[50];
+} Aluno;
+
+// Função para validar um aluno
+int validaAluno(Aluno alunos[], int total, char* nome) {
+    for(int i = 0; i < total; i++) {
+        if (strcmp(alunos[i].nome, nome) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int alunos() {
+    // Lista de alunos cadastrados
+    Aluno alunos[3] = {{"João"}, {"Maria"}, {"Pedro"}};
+    
+    // Nome do aluno a ser validado
+    char* nome = "Maria";
+    
+    // Chama a função para validar o aluno
+    int resultado = validaAluno(alunos, 3, nome);
+    
+    if (resultado != -1) {
+        printf("Aluno %s está cadastrado.\n", alunos[resultado].nome);
+    } else {
+        printf("Aluno não está cadastrado.\n");
+    }
+
+    return 0;
+}
+
