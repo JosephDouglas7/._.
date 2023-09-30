b2057c80021eae50520f66156eb71341ded6e39c
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-09-29 21:30:33 -0300

    validador_de_nome.c

---
+++ validador_de_nome.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int validar_nome(char* nome) {
+    int i = 0;
+    while (nome[i]) {
+        if (!isalpha(nome[i]) && nome[i] != ' ') {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int nome() {
+    char nome[100];
+    printf("Digite o seu nome: ");
+    fgets(nome, 100, stdin);
+
+    if (validar_nome(nome)) {
+        printf("Nome válido.\n");
+    } else {
+        printf("Nome inválido.\n");
+    }
+
+    return 0;
+}
+
