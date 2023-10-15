c63ab5ea6fdb9ef586079052e00c2cb42f71bbcc
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-10-15 15:50:42 -0300

    CPF.h

---
+++ CPF.h
@@ -0,0 +1,26 @@
+//Feito com a ajuda do Chat GPT
+#include <stdio.h>
+#include <string.h>
+
+// Definindo o tipo abstrato de dados CPF
+typedef struct {
+    char numero[12]; // 11 dígitos do CPF + 1 para o caractere nulo '\0'
+} CPF;
+
+// Função para inicializar o CPF
+void inicializaCPF(CPF *cpf, const char *numero) {
+    strncpy(cpf->numero, numero, sizeof(cpf->numero));
+}
+
+// Função para imprimir o CPF
+void imprimeCPF(CPF *cpf) {
+    printf("CPF: %s\n", cpf->numero);
+}
+
+int cpf() {
+    CPF meu_cpf;
+    inicializaCPF(&meu_cpf, "12345678901");
+    imprimeCPF(&meu_cpf);
+    return 0;
+}
+
