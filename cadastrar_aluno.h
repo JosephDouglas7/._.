7b87c53d51a9cd5188ff6f793cb002719a7e47cb
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2023-12-10 14:28:09 -0300

    Update cadastrar_aluno.h

--- cadastrar_aluno.h
+++ cadastrar_aluno.h
@@ -1,32 +1,52 @@
+#include<stdio.h> 
 #include <string.h> 
-#include<stdio.h>
 #include "util.h"
-struct estudante{ 
+typedef struct alunos alunos;
+
+struct alunos{ 
  long int matricula; 
  char nome[500]; 
  char nasc[10]; 
- char email[500];
+ char email[500]; 
+ char cpf [11]; 
+ char status;
 }; 
-void preenchealuno(struct estudante*); 
-void exibealuno(struct estudante);  
 
-int cadastrar(void) {
- estudante fulano;
- printf("\nCadastro de estudantes");
- preenchealuno(&fulano);
- exibealuno(fulano);
- return 0;
-}
-void preenchealuno(struct estudante * al) {
- printf("informe o nome do estudante:");
- printf("informe o CPF do aluno:");
- printf("informe a data de nascimento do aluno:"); 
- printf("informe o email do aluno:");  
- return; 
+
+alunos* preenchealuno(){ 
+ alunos*aln; 
+ aln = (alunos*)(sizeof(alunos)); 
+   printf("informe o nome do estudante:");
+   scanf("%ld",&aln->matricula);
+   printf("informe o CPF do aluno:"); 
+   scanf("%40[^\n]",aln->cpf);
+   printf("informe a data de nascimento do aluno:");  
+   scanf("%40[^\n]",aln->nasc);
+   printf("informe o email do aluno:");  
+   scanf("%40[^\n]",aln->email); 
+   aln -> status = 'm'; 
+   return aln;
 } 
-void exibealuno(const estudante al){ 
- printf("nome:%s\n",al.nome); 
- printf("email:%s\n", al.email); 
- printf("data de nascimento do aluno: %s\n",al.nasc);
- return; 
+char exibealuno(const alunos*al){ 
+  char situacao[20]; 
+  printf("\n= = = Aluno Cadastrado = = =\n");
+  printf("Aluno: %ld\n", al->matricula);
+  printf("CPF: %ld\n", al->matricula); 
+  printf("Data de nascimento: %ld\n", al->matricula); 
+  printf("Email: %ld\n", al->matricula); 
+  printf("Situação do aluno: %s\n", situacao);
+  return 0;
+} 
+int menuprincipal(void){ 
+  int op;
+   printf("\nMenu Principal\n");
+   printf("1 - Cadastrar aluno\n");
+   printf("2 - Pesquisar aluno\n");
+   printf("3 - Exibir aluno\n");
+   printf("0 - Encerrar programa\n");
+   printf("Escolha sua opção: ");
+   scanf("%d", &op);
+  return op;
 }
+
+
